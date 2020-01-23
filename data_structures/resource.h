e8ce8e29ac726628a7fd08f43a739d703802d97b
Samuel Maddox <smaddox@e1z3r1p20.42.us.org>
2020-01-22 22:21:11 -0800

    finished resource importer

--- data_structures/resource.h
+++ data_structures/resource.h
@@ -6,21 +6,32 @@
 /*   By: rpapagna <rpapagna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/19 16:59:54 by smaddox           #+#    #+#             */
-/*   Updated: 2020/01/22 16:56:41 by smaddox          ###   ########.fr       */
+/*   Updated: 2020/01/22 22:08:30 by smaddox          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RESOURCE_H
 # define RESOURCE_H
 
-#include "generic_structs.h"
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <dirent.h>
+# include <unistd.h>
+# include <errno.h>
+# include <stdlib.h>
+# include <string.h>
+# include <resource.h>
+# include <generic_structs.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <stdio.h>
 
 # define MAX_RESOURCE_COUNT 100
 
 typedef struct		s_resource
 {
 	int				size;
-	char			name[8]; // Max Length for resource filename
+	char			name[9]; // Max Length for resource filename
 	uint8_t			*raw_data;
 }					t_resource;
 
@@ -29,5 +40,7 @@ typedef struct		s_resource_table
 	size_t			size;
 	t_resource		table[MAX_RESOURCE_COUNT];
 }					t_resource_table;
+
+t_resource_table	get_resources(char *path);
 
 #endif
