bc4074d01172ec803d8b859f0a9edab969134c2e
Samuel Maddox <smaddox@e1z3r1p20.42.us.org>
2020-01-22 16:57:00 -0800

    add resource table to resource.h

--- data_structures/resource.h
+++ data_structures/resource.h
@@ -6,7 +6,7 @@
 /*   By: rpapagna <rpapagna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/19 16:59:54 by smaddox           #+#    #+#             */
-/*   Updated: 2020/01/20 17:54:48 by rpapagna         ###   ########.fr       */
+/*   Updated: 2020/01/22 16:56:41 by smaddox          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,19 @@
 
 #include "generic_structs.h"
 
+# define MAX_RESOURCE_COUNT 100
+
 typedef struct		s_resource
 {
 	int				size;
 	char			name[8]; // Max Length for resource filename
 	uint8_t			*raw_data;
 }					t_resource;
+
+typedef struct		s_resource_table
+{
+	size_t			size;
+	t_resource		table[MAX_RESOURCE_COUNT];
+}					t_resource_table;
 
 #endif
