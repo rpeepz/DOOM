5985cdebb0a956534efcf5e1b1a439d74c855820
Samuel Maddox <smaddox@e1z3r1p20.42.us.org>
2020-02-06 18:15:25 -0800

    same thing

--- inc/list.h
+++ inc/list.h
@@ -6,11 +6,12 @@
 /*   By: smaddox <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/23 22:13:43 by smaddox           #+#    #+#             */
-/*   Updated: 2020/02/05 16:13:57 by smaddox          ###   ########.fr       */
+/*   Updated: 2020/02/05 20:22:40 by smaddox          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "map.h"
+//#include "nuklear.h"
 
 typedef struct		s_line_node{
 	t_linedef		line;
@@ -20,9 +21,9 @@ typedef struct		s_line_node{
 }					t_line_node;
 
 typedef struct		s_line_bank{
+	int				empty;
 	int				count;
 	t_line_node 	*selected;
-	t_line_node		*iter;
 	t_line_node		*head;
 	t_line_node 	*tail;
 }					t_line_bank;
