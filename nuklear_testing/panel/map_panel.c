8c0949e53c5e8b5bbb61308c311675a6dbf07e34
rpeepz <r.papagna.42@gmail.com>
2020-02-18 03:41:02 -0800

    Added small function to file tool

--- nuklear_testing/panel/map_panel.c
+++ nuklear_testing/panel/map_panel.c
@@ -18,9 +18,14 @@ static struct nk_rect cursor = { .w = 4, .h = 4};
 static struct nk_rect end_point = { .w = 4, .h = 4};
 static int count = 0;
 static int count2 = 0;
+static int show_grid = nk_true;
+static int show_about = nk_false;
 
 void    draw_lines(t_map_interface *draw_mode);
 void    draw_things(t_map_interface *draw_mode);
+void    draw_grid(struct nk_rect size);
+void    draw_menu(t_map_interface *draw_mode);
+void    draw_about(struct nk_context *ctx);
 
 void    map_pannel(t_map_interface *draw_mode)
 {
@@ -35,31 +40,7 @@ void    map_pannel(t_map_interface *draw_mode)
 		NK_WINDOW_BORDER|NK_WINDOW_NO_SCROLLBAR|NK_WINDOW_MINIMIZABLE))
 		{
 			canvas = nk_window_get_canvas(draw_mode->ctx);
-			size = nk_window_get_content_region(draw_mode->ctx);
-
-			float x, y;
-			const float grid_size = 10.0f;
-			const struct nk_color grid_color = nk_rgba(35, 35, 35, 150);
-			const struct nk_color grid_color_2 = nk_rgb(50, 50, 50);
-			const struct nk_color grid_color_3 = nk_rgb(30, 30, 30);
-			
-            /* sub grid */
-            for (x = grid_size; x < size.w; x += grid_size * 2)
-				nk_stroke_line(canvas, x+size.x, size.y, x+size.x, size.y+size.h, 1.0f, grid_color);
-			for (y = (float)fmod(size.y, grid_size) + grid_size; y < size.h; y += grid_size * 2)
-				nk_stroke_line(canvas, size.x, y+size.y, size.x+size.w, y+size.y, 1.0f, grid_color);
-
-            /* inner grid */
-            for (x = 0; x < size.w; x += grid_size * 2)
-				nk_stroke_line(canvas, x+size.x, size.y, x+size.x, size.y+size.h, 1.0f, grid_color_2);
-			for (y = (float)fmod(size.y, grid_size); y < size.h; y += grid_size * 2)
-				nk_stroke_line(canvas, size.x, y+size.y, size.x+size.w, y+size.y, 1.0f, grid_color_2);
-
-            /* outer grid */
-			for (x = 0; x < size.w; x += grid_size*grid_size)
-                nk_stroke_line(canvas, x+size.x, size.y, x+size.x, size.y+size.h, 1.5f, grid_color_3);
-			for (y = -3.5; y < size.h; y += grid_size*grid_size)
-                nk_stroke_line(canvas, size.x, y+size.y, size.x+size.w, y+size.y, 1.5f, grid_color_3);
+            if (show_grid) draw_grid(nk_window_get_content_region(draw_mode->ctx));
 
             SDL_ShowCursor(SDL_ENABLE);
             if ((draw_mode->tool_op) == LINE) {
@@ -72,10 +53,16 @@ void    map_pannel(t_map_interface *draw_mode)
                 nk_fill_circle(canvas, cursor, nk_rgb(0, 255, 0));
                 draw_things(draw_mode);
             }
+            /* fill items on map */
             for (t_item_node *temp = draw_mode->bank->head_line; temp; temp = temp->next)
                 stroke_my_line(canvas, temp);
             for (t_item_node *temp = draw_mode->bank->head_thing; temp; temp = temp->next)
                 stroke_box(canvas, temp);
+            /* menu bar */
+            if (draw_mode->tool_op == NK_FILE) draw_menu(draw_mode);
+            /* about map-maker */
+            if (show_about) draw_about(draw_mode->ctx);
+        
 		}
 		nk_end(draw_mode->ctx);
 }
@@ -193,4 +180,94 @@ void    draw_things(t_map_interface *draw_mode)
             }
         }
     }
+}
+
+void    draw_grid(struct nk_rect size)
+{
+    float x, y;
+    const float grid_size = 10.0f;
+    const struct nk_color grid_color = nk_rgba(35, 35, 35, 150);
+    const struct nk_color grid_color_2 = nk_rgb(50, 50, 50);
+    const struct nk_color grid_color_3 = nk_rgb(30, 30, 30);
+    
+    /* sub grid */
+    for (x = grid_size; x < size.w; x += grid_size * 2)
+        nk_stroke_line(canvas, x+size.x, size.y, x+size.x, size.y+size.h, 1.0f, grid_color);
+    for (y = (float)fmod(size.y, grid_size) + grid_size; y < size.h; y += grid_size * 2)
+        nk_stroke_line(canvas, size.x, y+size.y, size.x+size.w, y+size.y, 1.0f, grid_color);
+
+    /* inner grid */
+    for (x = 0; x < size.w; x += grid_size * 2)
+        nk_stroke_line(canvas, x+size.x, size.y, x+size.x, size.y+size.h, 1.0f, grid_color_2);
+    for (y = (float)fmod(size.y, grid_size); y < size.h; y += grid_size * 2)
+        nk_stroke_line(canvas, size.x, y+size.y, size.x+size.w, y+size.y, 1.0f, grid_color_2);
+
+    /* outer grid */
+    for (x = 0; x < size.w; x += grid_size*grid_size)
+        nk_stroke_line(canvas, x+size.x, size.y, x+size.x, size.y+size.h, 1.5f, grid_color_3);
+    for (y = -3.5; y < size.h; y += grid_size*grid_size)
+        nk_stroke_line(canvas, size.x, y+size.y, size.x+size.w, y+size.y, 1.5f, grid_color_3);
+}
+
+void    draw_menu(t_map_interface *draw_mode)
+{
+    struct nk_context *ctx = draw_mode->ctx;
+    nk_menubar_begin(ctx);
+    nk_layout_row_begin(ctx, NK_STATIC, 25, 2);
+    nk_layout_row_push(ctx, 40);
+    if (nk_menu_begin_label(ctx, "MENU", NK_TEXT_LEFT, nk_vec2(120, 200)))
+    {
+        nk_layout_row_dynamic(ctx, 25, 1);
+        if (nk_menu_item_label(ctx, "Hide menu", NK_TEXT_LEFT)) {
+            draw_mode->tool_op = LINE;
+            count = -1;
+        }
+        if (nk_menu_item_label(ctx, "About", NK_TEXT_LEFT))
+            show_about = nk_true;
+        nk_checkbox_label(ctx, "Show grid", &show_grid);
+        nk_menu_end(ctx);
+    }
+    nk_layout_row_push(ctx, 60);
+    if (nk_menu_begin_label(ctx, "ADVANCED", NK_TEXT_LEFT,
+    nk_vec2(120, 200)))
+    {
+        nk_layout_row_dynamic(ctx, 25, 1);
+        if (nk_menu_item_label(ctx, "About", NK_TEXT_LEFT))
+            ;
+        if (nk_menu_item_label(ctx, "Template", NK_TEXT_LEFT))
+            ;
+        if (nk_menu_item_label(ctx, "Blank", NK_TEXT_LEFT))
+            ;
+        nk_menu_end(ctx);
+    }
+}
+
+void    draw_about(struct nk_context *ctx)
+{
+    static struct nk_rect s = {20, 100, 300, 250};
+    if (nk_popup_begin(ctx, NK_POPUP_STATIC, "About", NK_WINDOW_CLOSABLE, s))
+    {
+        nk_layout_row_begin(ctx, NK_STATIC, 25, 3);
+        nk_layout_row_push(ctx, 50);
+        nk_label(ctx, " ", NK_TEXT_CENTERED);
+        nk_layout_row_push(ctx, 70);
+        nk_label_colored(ctx, "DOOM ", NK_TEXT_RIGHT, nk_rgb(255,0,0));
+        nk_label(ctx, " Map-Maker", NK_TEXT_LEFT);
+        nk_layout_row_end(ctx);
+        nk_layout_row_dynamic(ctx, 20, 2);
+        nk_label(ctx, "Leader:", NK_TEXT_LEFT);
+        nk_label(ctx, "Samuel Maddox", NK_TEXT_RIGHT);
+        nk_label(ctx, "Designer/Developer:", NK_TEXT_LEFT);
+        nk_label(ctx, "Robert Papagna", NK_TEXT_RIGHT);
+        nk_label(ctx, "Research/Info:", NK_TEXT_LEFT);
+        nk_label(ctx, "Steven Limon", NK_TEXT_RIGHT);
+        nk_label(ctx, "Support:", NK_TEXT_LEFT);
+        nk_label(ctx, "Francisco Guzman", NK_TEXT_RIGHT);
+
+        nk_layout_row_dynamic(ctx, 10, 1);
+        nk_label(ctx, " ", NK_TEXT_CENTERED);
+        nk_layout_row_dynamic(ctx, 60, 1);
+        nk_label_wrap(ctx, "nuklear is licensed under the public domain License.");
+        nk_popup_end(ctx);
+    } else show_about = nk_false;
 }
